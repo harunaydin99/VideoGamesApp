fea567390849c5c1fbcf104beee8b619baf122f8
Harun Aydın <harunaydin99@gmail.com>
2021-09-13 15:17:35 +0300

    Initial Commit

---
+++ VideoGamesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VideoGamesApp
+//
+//  Created by Harun AYDIN on 13.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
